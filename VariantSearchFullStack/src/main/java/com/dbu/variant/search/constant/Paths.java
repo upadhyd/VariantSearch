972ab36f66f057fe637af1eb3e44a9b3de35e851
upadhyd <upadhyd@gmail.com>
2019-02-24 06:25:07 -0500

    Adding test for both react and spring-boot projects

---
+++ VariantSearchFullStack/src/main/java/com/dbu/variant/search/constant/Paths.java
@@ -0,0 +1,8 @@
+package com.dbu.variant.search.constant;
+
+public class Paths {
+
+  public static final String VERSION = "/api";
+  public static final String VARIANT = "/variant";
+  public static final String SEARCH = "/search";
+}
